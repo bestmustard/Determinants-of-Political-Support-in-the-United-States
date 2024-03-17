f3e1a01eda237c87b8942ba31b74513a9be5209c
Victor Ma <victorjjma@gmail.com>
2024-03-16 23:41:52 -0400

    Create 04-model.R

---
+++ scripts/04-model.R
@@ -0,0 +1,38 @@
+#### Preamble ####
+# Purpose: Creates a logistic regression model
+# Author: Victor Ma
+# Date: 15 Mar 2024
+# Contact: victo.ma@mail.utoronto.ca
+# License: MIT
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(rstanarm)
+
+#### Read data ####
+ces_data <- read_csv("data/analysis_data/analysis_data.csv")
+
+ces_data$voted_for <- as_factor(ces_data$voted_for)
+ces_data$education <- factor(ces_data$education, levels = c("No HS", "High school graduate", "Some college", "2-year", "4-year", "Post-grad"))
+### Model data ####
+set.seed(853)
+
+# Reducing the CES 2020 dataset for manageable computation
+ces_data_reduced <- ces_data |> 
+  slice_sample(n = 3000)
+
+
+# Specifying the logistic regression model
+political_preferences <- stan_glm(
+  voted_for ~ age + education + gender,
+  data = ces_data_reduced,
+  family = binomial(link = "logit"),
+  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  seed = 853
+)
+
+# Saving and loading the model for future analysis
+saveRDS(political_preferences, file = "models/political_preferences.rds")
+
