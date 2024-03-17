41f483ec69b00eaec79eb92be0867c7dad7568dc
Victor Ma <victorjjma@gmail.com>
2024-03-16 23:41:32 -0400

    Create 01-download_data.R

---
+++ scripts/01-download_data.R
@@ -0,0 +1,26 @@
+#### Preamble ####
+# Purpose: Downloads and saves the 2020 CCES data recorded by Schaffner, Brian et.al 
+# Author: Victor Ma
+# Date: 15 Mar 2024
+# Contact: victo.ma@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+
+#install.packages("arrow")
+library(tidyverse)
+library(dataverse)
+library(arrow) # Add this for parquet file support
+library(dplyr)
+
+#### Download data ####
+ces2020 <-
+  get_dataframe_by_name(
+    filename = "CES20_Common_OUTPUT_vv.csv",
+    dataset = "10.7910/DVN/E9N6PH",
+    server = "dataverse.harvard.edu",
+    .f = read_csv
+  ) 
+
+#### Save data ####
+write_parquet(ces2020, "data/raw_data/raw_data.parquet")
